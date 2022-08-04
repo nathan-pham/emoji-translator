f824cc4562886d99a210390e2cfbaa5a4e8fdd12
Nathan Pham <43102813+nathan-pham@users.noreply.github.com>
2022-08-04 18:49:40 +0000

    initial commit: full translator

---
+++ index.js
@@ -0,0 +1,25 @@
+const emojilib = require("emojilib")
+
+const findEmojiSynonym = (word) => {
+    const preprocessedWord = word
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+
+    for (const [emoji, synonyms] of Object.entries(emojilib)) {
+
+        if (synonyms.includes(preprocessedWord)) {
+            return emoji
+        }
+    }
+
+    return word
+}
+
+const translate = (sentence) => (
+    sentence
+        .split(" ")
+        .map(findEmojiSynonym)
+        .join(' ')
+)
+
+console.log(translate("Hello!"))
